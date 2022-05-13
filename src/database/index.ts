8df3fb0ed45a14ca2ee9925055e1ed61927aa6bb
almeidadylan <almeidadylan793@gmail.com>
2022-05-13 16:07:43 -0400

    testando imagens

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@
 export const products = [
     {  
         id: 1,
-        imagem: "../imagens/panquecas.svg",
+        imagem: "panquecas.svg",
         nome: "Panqueca de banana com aveia",
         descricao: "Esta receita serve muito bem 2 pessoas, deixa a gente bem satisfeito.",
         categoria: "Panificadora",
@@ -10,7 +10,7 @@ export const products = [
     },
     {  
         id: 2,
-        imagem: "../imagens/mousse_de_morango.svg",
+        imagem: "mousse_de_morango.svg",
         nome: "Mousse de morango com a fruta",
         descricao: "Sobremesa fácil, rápida e muito saborosa: o mousse.",
         categoria: "Frutas",
@@ -18,7 +18,7 @@ export const products = [
     },
     {  
         id: 3,
-        imagem: "../imagens/pastel_vegano.svg",
+        imagem: "pastel_vegano.svg",
         nome: "Pastel de verduras vagano",
         descricao: "Que tal comer um delicioso pastel feito de vegetais cozidos no vapor.",
         categoria: "Panificadora",
@@ -26,7 +26,7 @@ export const products = [
     },
     {  
         id: 4,
-        imagem: "../imagens/pizza_vegetal.svg",
+        imagem: "pizza_vegetal.svg",
         nome: "Pizza vegetariana de palmito",
         descricao: "Uma saborosa pizza de massa fina feita de palmito ralado, com cebola, ovos cozidos, oregano e manjericão.",
         categoria: "Panificadora",
@@ -34,7 +34,7 @@ export const products = [
     },
     {  
         id: 5,
-        imagem: "../imagens/vinho.svg",
+        imagem: "vinho.svg",
         nome: "Vinho suave",
         descricao: "O vinho tinto seco. De corpo médio cheio de sabores de frutas vermelhas maduras e delicadas notas de especiarias.",
         categoria: "Bebidas",
@@ -42,7 +42,7 @@ export const products = [
     },
     {  
         id: 6,
-        imagem: "../imagens/laranja.svg",
+        imagem: "laranja.svg",
         nome: "Pizza vegetariana de palmito",
         descricao: "Bastante popular no Brasil, a laranja é uma das maiores representantes das frutas cítricas. Seu sabor costuma ser doce ou levemente ácido.",
         categoria: "Frutas",
