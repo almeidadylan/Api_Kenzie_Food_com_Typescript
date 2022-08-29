9f71f50498451a2942a731f59948be1545422b05
almeidadylan <almeidadylan793@gmail.com>
2022-08-29 09:22:14 -0400

    fix: bug yarn start

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,24 +2,22 @@ import { DataSource } from "typeorm";
 import "dotenv/config";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    synchronize: false,
-    logging: true,
-    entities: ["src/models/*.ts"],
-    migrations: ["src/migrations/*.ts"],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  logging: true,
+  entities: ["src/models/*.ts"],
+  migrations: ["src/migrations/*.ts"],
+});
 
 AppDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-
-
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
