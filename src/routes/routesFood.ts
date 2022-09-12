7a0fc3f08a05ad4503fa5ff3141f3600881b287e
almeidadylan <almeidadylan793@gmail.com>
2022-09-11 20:31:45 -0400

    feat: add route patch - update products

--- src/routes/routesFood.ts
+++ src/routes/routesFood.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import CreateProductController from "../controllers/createProduct.controller";
 import listProductsController from "../controllers/listProducts.controller";
+import UpdateProductController from "../controllers/updateProduct.controller";
 
 const routes = Router();
 
 routes.get("/products", listProductsController)
 routes.post("/products", CreateProductController)
+routes.patch("/products/:id", UpdateProductController)
 
 export default routes
